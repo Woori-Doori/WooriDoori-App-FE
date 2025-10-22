79b9d3b97d3a7ba12b7fc3e5841428dae74da778
songhajang <angpan2004@gmail.com>
2025-10-22 17:45:07 +0900

    [fix] #1 - 공통 div padding & border 추가

--- src/components/default/DefaultDiv.tsx
+++ src/components/default/DefaultDiv.tsx
@@ -1,6 +1,6 @@
 const DefaultDiv = ({ children }: { children: React.ReactNode}) =>{
     return (
-        <div className="overflow-auto relative min-h-[100vh] h-[100vh] m-auto bg-white font-sans dark:bg-gray-700" style={{width:'400px'}}>
+        <div className="border overflow-auto relative min-h-[100vh] h-[100vh] m-auto bg-white font-sans dark:bg-gray-700 p-10" style={{width:'400px'}}>
             {children}
         </div>
     )
